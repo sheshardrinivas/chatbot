62113f6d468aa11002eae5efab2eaa80716de2c8
sheshadrinivas <127858531+sheshardrinivas@users.noreply.github.com>
2025-11-29 21:27:59 +0530

    animations like apple

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,9 @@
-import Image from "next/image";
-
-
 import Div from "./components/Div"
 
 export default async function Home() {
     return (
         <div
+
             className="flex h-screen items-center justify-center  font-sans   ">
             <Div/>
         </div>
